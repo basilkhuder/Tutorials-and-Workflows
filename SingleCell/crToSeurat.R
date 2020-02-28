c42b6e7bd0581b5907ab31a9e4b8ce8b208f405d
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-02-27 18:08:25 -0600

    Create crToSeurat.R

---
+++ SingleCell/crToSeurat.R
@@ -0,0 +1,29 @@
+#Creates Seurat objects from CellRanger Folders
+
+crToSeurat <- function(directory){ 
+  
+  folders <- list.dirs(directory,recursive = FALSE)
+  matrix.list <- vector(mode = "list", length = length(folders))
+  features.list <- vector(mode = "list", length = length(folders))
+  barcode.list <- vector(mode = "list", length = length(folders))
+  
+  for (i in 1:length(folders)){ 
+    filtered.folder <- list.files(path = folders[[i]], pattern = "filtered")
+    full.dir <- paste(folders[[i]],filtered.folder,sep = "/")
+    
+    matrix.list[[i]] <- Matrix::readMM(paste(full.dir,list.files(path = full.dir, pattern = "matrix"),sep = "/"))
+    
+    features.list[[i]] <- read.delim(paste(full.dir,list.files(path = full.dir, pattern = "features"),sep = "/"), 
+                                     header = FALSE,
+                                     stringsAsFactors = FALSE)
+    
+    barcode.list[[i]] <- read.delim(paste(full.dir,list.files(path = full.dir, pattern = "barcode"),sep = "/"),
+                                    header = FALSE,
+                                    stringsAsFactors = FALSE)
+    
+    colnames(matrix.list[[i]]) <- barcode.list[[i]]$V1
+    rownames(matrix.list[[i]]) <- features.list[[i]]$V1
+    matrix.list[[i]] <- Seurat::CreateSeuratObject(matrix.list[[i]])
+  }
+  return(matrix.list)
+}
